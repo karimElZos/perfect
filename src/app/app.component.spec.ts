624e3b78853d6e87840ecf732e9b1a3f11da06c4
karimElZos <81989794+karimElZos@users.noreply.github.com>
2021-05-03 18:32:53 +0200

    Initial Commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -4,9 +4,7 @@ import { AppComponent } from './app.component';
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
+      declarations: [AppComponent],
     }).compileComponents();
   });
 
@@ -16,16 +14,18 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'protmp'`, () => {
+  it(`should have as title 'Perfect Zoser Template'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('protmp');
+    expect(app.title).toEqual('Perfect Zoser Template');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('protmp app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain(
+      'Perfect Zoser Template app is running!'
+    );
   });
 });
